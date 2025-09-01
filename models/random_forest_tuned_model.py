38e505e74628eefba980540e48b7193fcc629614
Btaykb <btayykb@gmail.com>
2025-09-01 16:03:45 +0800

    add tuning for random forest regressor

--- models/random_forest_tuned_model.py
+++ models/random_forest_tuned_model.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.compose import ColumnTransformer
 from sklearn.metrics import r2_score
-from data_preprocessing import load_data, encode_categorical
+from sklearn.model_selection import RandomizedSearchCV
 
 df = fetch_california_housing(as_frame=True)
 X = df.data
@@ -24,8 +24,25 @@ pipeline = Pipeline(steps=[
     ('regressor', model)
 ])
 
-pipeline.fit(X_train, y_train)
+# Include model tuning
 
-y_pred = pipeline.predict(X_test)
-score = r2_score(y_test, y_pred)
-print(score)
+param_grid = {
+    "regressor__n_estimators": [100, 200, 400, 500],
+    "regressor__max_depth": [None, 10, 20, 30],
+    "regressor__min_samples_split": [2, 5],
+    "regressor__min_samples_leaf": [1, 2]
+}
+
+grid_search = RandomizedSearchCV(
+    estimator=pipeline,
+    param_distributions=param_grid,
+    cv=5,
+    n_jobs=-1,
+    n_iter=50,
+    verbose=2
+)
+
+grid_search.fit(X_train, y_train)
+y_pred_tuned = grid_search.predict(X_test)
+score_tuned = r2_score(y_test, y_pred_tuned)
+print(f"R2 Score for tuned model: {score_tuned}")
