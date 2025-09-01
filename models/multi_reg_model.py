97a888fd70d58f586ece8a2ee0e7cc8fa062aba3
Btaykb <btayykb@gmail.com>
2025-09-01 17:14:44 +0800

    add base multi reg model

---
+++ models/multi_reg_model.py
@@ -0,0 +1,30 @@
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.compose import ColumnTransformer
+from sklearn.metrics import r2_score
+
+df = fetch_california_housing(as_frame=True)
+X = df.data
+y = df.target
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+preprocessor = ColumnTransformer(transformers=[
+    ('num', StandardScaler(), X.columns)
+])
+
+model = LinearRegression()
+
+pipeline = Pipeline(steps=[
+    ('regressor', model)
+])
+
+pipeline.fit(X_train, y_train)
+
+y_pred = pipeline.predict(X_test)
+score = r2_score(y_test, y_pred)
+print(score)
